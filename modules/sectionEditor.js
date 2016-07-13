919d84e04962b32b66e8fd626ddf71d9e4087142
Alessandro Tundo <alessandrotundo94@gmail.com>
2016-07-13 19:48:19 +0200

    Added edit chapter name function
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- modules/sectionEditor.js
+++ modules/sectionEditor.js
@@ -38,35 +38,99 @@ function DraggableHandledItemWidget( config ) {
 OO.inheritClass( DraggableHandledItemWidget, OO.ui.DecoratedOptionWidget );
 OO.mixinClass( DraggableHandledItemWidget, OO.ui.mixin.DraggableElement );
 
+function EditDialog(draggableWidget, chapterName, config ) {
+    EditDialog.parent.call( this, config );
+    this.draggableWidget = draggableWidget;
+    this.chapterName = chapterName;
+    this.textInputWidget = new OO.ui.TextInputWidget($.extend( { validate: 'non-empty' }, config ) );
+    this.textInputWidget.setValue(chapterName);
+}
+OO.inheritClass( EditDialog, OO.ui.ProcessDialog );
+
+EditDialog.static.title = 'Edit Dialog';
+EditDialog.static.actions = [
+    { action: 'save', label: 'Rename', flags: 'primary' },
+    { label: 'Cancel', flags: 'safe' }
+];
+
+EditDialog.prototype.initialize = function () {
+    EditDialog.parent.prototype.initialize.apply( this, arguments );
+    this.content = new OO.ui.PanelLayout( { padded: true, expanded: false } );
+    this.content.$element.append(this.textInputWidget.$element );
+    this.$body.append( this.content.$element );
+};
+
+EditDialog.prototype.getActionProcess = function ( action ) {
+    var dialog = this;
+    if ( action === 'save' ) {
+        return new OO.ui.Process( function () {
+            var newChapterName = dialog.textInputWidget.getValue();
+            var items = dialog.draggableWidget.getItems();
+            items.filter(function(chapter) {
+              if(chapter.data === dialog.chapterName){
+                chapter.setData(newChapterName);
+                chapter.setLabel(newChapterName);
+                var iconDelete = $("<i class='fa fa-trash deleteChapterIcon pull-right'></i>");
+                var iconEdit = $("<i class='fa fa-pencil editChapterIcon pull-right'></i>");
+                chapter.$label.append(iconDelete, iconEdit);
+                $(iconDelete).click(function(){
+                  deleteChapter(dialog.draggableWidget, $(this).parent().text());
+                });
+                $(iconEdit).click(function(){
+                  editChapter(dialog.draggableWidget, $(this).parent().text());
+                });
+              }
+            });
+            dialog.close( { action: action } );
+        } );
+    }
+    return EditDialog.parent.prototype.getActionProcess.call( this, action );
+};
+
+
 var deleteChapter = function(draggableWidget, chapterName){
   var chapterToRemove = draggableWidget.getItemFromData(chapterName);
   draggableWidget.removeItems([chapterToRemove]);
 };
 
 var addChapter = function(draggableWidget, chapterName){
-  var dragItem = new DraggableHandledItemWidget( {
-    data: chapterName,
-    icon: 'menu',
-    label: chapterName
-  } );
-  var iconDelete = $("<i class='fa fa-trash deleteChapterIcon pull-right'></i>");
-  dragItem.$label.append(iconDelete);
-  draggableWidget.addItems([dragItem]);
-  $(iconDelete).click(function(){
-    deleteChapter(draggableWidget, $(this).parent().text());
-  });
+  if(chapterName !== ''){
+    var dragItem = new DraggableHandledItemWidget( {
+      data: chapterName,
+      icon: 'menu',
+      label: chapterName
+    } );
+    var iconDelete = $("<i class='fa fa-trash deleteChapterIcon pull-right'></i>");
+    var iconEdit = $("<i class='fa fa-pencil editChapterIcon pull-right'></i>");
+    dragItem.$label.append(iconDelete, iconEdit);
+    draggableWidget.addItems([dragItem]);
+    $(iconDelete).click(function(){
+      deleteChapter(draggableWidget, $(this).parent().text());
+    });
+    $(iconEdit).click(function(){
+      editChapter(draggableWidget, $(this).parent().text());
+    });
+  }
+};
+
+var editChapter = function(draggableWidget, chapterName){
+  var windowManager = new OO.ui.WindowManager();
+  $('body').append( windowManager.$element );
+  var dialog = new EditDialog(draggableWidget, chapterName);
+  windowManager.addWindows( [ dialog ] );
+  windowManager.openWindow( dialog );
 };
 
 $(function () {
   var dragChapters = [];
-  //var chapters = $("li[class='chapterItem']");
   $.each(chapters, function(key, value){
     var dragItem = new DraggableHandledItemWidget( {
       data: value,
       icon: 'menu',
       label: value
     } );
-    dragItem.$label.append("<i class='fa fa-trash deleteChapterIcon pull-right'></i>");
+    dragItem.$label.append("<i class='fa fa-trash deleteChapterIcon pull-right'></i>",
+    "<i class='fa fa-pencil editChapterIcon pull-right'></i>");
     dragChapters.push(dragItem);
   });
   var draggableWidget = new DraggableGroupWidget( {
@@ -85,6 +149,9 @@ $(function () {
   $('.oo-ui-inputWidget-input').attr('id', 'addChapter');
   $('.deleteChapterIcon').click(function(){
     deleteChapter(draggableWidget, $(this).parent().text());
+  });
+  $('.editChapterIcon').click(function(){
+    editChapter(draggableWidget, $(this).parent().text());
   });
 
   $('#addChapter').blur(function(){
